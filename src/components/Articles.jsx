e3662f10b9a052653a171128b5ddc0941f4874c0
rjg55 <rikgraham55@gmail.com>
2022-08-25 13:55:55 +0100

    add comment count to articles page

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -50,7 +50,7 @@ const Articles = () => {
         </section>
         <section>
             <ul className="articles--card_list">
-                {articles.map(({article_id, title, author, topic, created_at, votes}) => {
+                {articles.map(({article_id, title, author, topic, created_at, votes, comment_count}) => {
                     return (
                         <li className="article--card" key={article_id}>
                             <h3 className="article--card--title">{title}</h3>
@@ -58,6 +58,7 @@ const Articles = () => {
                             <h3 className="article--card--topic">Topic: {topic}</h3>
                             <h3 className="article--card--created_at">Posted: {created_at.substring(0,10)}</h3>
                             <h3 className="article--card--votes">Votes: {votes === 0 ? "Be the first to upvote this!": votes}</h3>
+                            <h3 className="article--card--comments"> Comments: {comment_count}</h3>
                             <Link to={`/articles/${article_id}`}><button className="article--card--button">View</button></Link>
                         </li>
                     )
