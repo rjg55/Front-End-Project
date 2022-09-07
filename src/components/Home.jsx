8fbf9f7145fddabc2001facef59c11303a1d1558
rjg55 <rikgraham55@gmail.com>
2022-09-07 22:01:58 +0100

    add home page articles and intro and css styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,46 @@
+import { useState, useContext } from "react";
+import { UserContext } from "../context/User";
+import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import { fetchArticles } from "../api functions/api";
+
 const Home = () => {
+
+    const {loggedInUser} = useContext(UserContext)
+    const [articles, setArticles] = useState([])
+
+    useEffect(() => {
+        fetchArticles().then((articlesFromApi) => {
+            setArticles(articlesFromApi.data.articles)
+        })
+    }, [])
+
     return (
         <>
-        <section className="home--intro">Intro blurb - welcome USERNAME </section>
+        <section className="home--intro">{`Hi ${loggedInUser.username}, welcome to WTAN! We're a news website dedicated to bringing you latest, greatest and unbiased news this side of the web!`}</section>
         <br />
+        <p className="home--intro">Browse some highlighted articles below:</p>
         <div className="home--articles_collection">
-            <section className="home--article_card">
-                example article card
-            </section>
-        </div>
+        <section className="home--article_card">
+            <ul className="articles--card_list">
+                {articles.map(({article_id, title, author, topic, created_at, votes, comment_count}, index) => {
+                    if (index < 6) {
+                        return (
+                            <li className="article--card" key={article_id}>
+                            <h3 className="article--card--title">{title}</h3>
+                            <h3 className="article--card--author">Author: {author}</h3>
+                            <h3 className="article--card--topic">Topic: {topic}</h3>
+                            <h3 className="article--card--created_at">Posted: {created_at.substring(0,10)}</h3>
+                            <h3 className="article--card--votes">Votes: {votes === 0 ? "Be the first to upvote this!": votes}</h3>
+                            <h3 className="article--card--comments"> Comments: {comment_count}</h3>
+                            <Link to={`/articles/${article_id}`}><button className="article--card--button">View</button></Link>
+                        </li>
+                    )
+                }
+                })}
+                </ul>
+                </section>
+                </div>
         </>
     )
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,11 +2,11 @@ import {Link} from "react-router-dom"
 
 const Nav = () => {
     return (
-        <nav>
-            <Link to="/">Home</Link>
-            <Link to="/users">Users</Link>
-            <Link to="/articles">Articles</Link>
-            <Link to="/topics">Topics</Link>
+        <nav className="nav--bar">
+            <Link className="nav--link--home" to="/">Home</Link>
+            <Link className="nav--link--users" to="/users">Users</Link>
+            <Link className="nav--link--articles" to="/articles">Articles</Link>
+            <Link className="nav--link--topics" to="/topics">Topics</Link>
         </nav>
     )
 }
