98fdf9fa9cd8b4b86d1870566be3f3664eee1ec4
rjg55 <106883089+rjg55@users.noreply.github.com>
2022-11-16 21:51:07 +0000

    update all axios request urls to new back-end

--- src/api functions/api.js
+++ src/api functions/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const fetchArticles = (sortby, order) => {
   return axios
-    .get(`https://wtan.herokuapp.com/api/articles`, {
+    .get(`https://wtan.cyclic.app/api/articles`, {
       params: { sortby, order },
     })
     .then((res) => {
@@ -12,21 +12,21 @@ export const fetchArticles = (sortby, order) => {
 
 export const fetchArticlesById = (article_id) => {
   return axios
-    .get(`https://wtan.herokuapp.com/api/articles/${article_id}`)
+    .get(`https://wtan.cyclic.app/api/articles/${article_id}`)
     .then((res) => {
       return res;
     });
 };
 
 export const fetchTopics = () => {
-  return axios.get("https://wtan.herokuapp.com/api/topics").then((res) => {
+  return axios.get("https://wtan.cyclic.app/api/topics").then((res) => {
     return res;
   });
 };
 
 export const fetchArticlesByTopic = (sortby, order, topic) => {
   return axios
-    .get(`https://wtan.herokuapp.com/api/articles?topic=${topic}`, {
+    .get(`https://wtan.cyclic.app/api/articles?topic=${topic}`, {
       params: { sortby, order },
     })
     .then((res) => {
@@ -35,20 +35,20 @@ export const fetchArticlesByTopic = (sortby, order, topic) => {
 };
 
 export const patchVotesById = (article_id, vote_delta) => {
-  return axios.patch(`https://wtan.herokuapp.com/api/articles/${article_id}`, {
+  return axios.patch(`https://wtan.cyclic.app/api/articles/${article_id}`, {
     inc_votes: vote_delta,
   });
 };
 
 export const fetchUsers = () => {
-  return axios.get("https://wtan.herokuapp.com/api/users").then((res) => {
+  return axios.get("https://wtan.cyclic.app/api/users").then((res) => {
     return res;
   });
 };
 
 export const fetchCommentsByArticle = (article_id) => {
   return axios
-    .get(`https://wtan.herokuapp.com/api/articles/${article_id}/comments`)
+    .get(`https://wtan.cyclic.app/api/articles/${article_id}/comments`)
     .then((response) => {
       return response;
     });
@@ -56,7 +56,7 @@ export const fetchCommentsByArticle = (article_id) => {
 
 export const postCommentByArticle = (article_id, author, comment_body) => {
   return axios
-    .post(`https://wtan.herokuapp.com/api/articles/${article_id}/comments`, {
+    .post(`https://wtan.cyclic.app/api/articles/${article_id}/comments`, {
       username: author,
       body: comment_body,
     })
@@ -67,7 +67,7 @@ export const postCommentByArticle = (article_id, author, comment_body) => {
 
 export const deleteCommentByID = (comment_id) => {
   return axios
-    .delete(`https://wtan.herokuapp.com/api/comments/${comment_id}`)
+    .delete(`https://wtan.cyclic.app/api/comments/${comment_id}`)
     .then((res) => {
       return res;
     });
